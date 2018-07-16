d924a0c673a13a0bb36ff3e1e88655eddb9bbb6e
guilherme <gui11_3@hotmail.com>
2018-07-15 23:48:43 -0300

    modulo 4 webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,52 @@
+const path =require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins=[];
+
+if(process.env.NODE_ENV =='production'){
+    plugins.push(new babiliPlugin());
+}
+
+module.exports={
+    entry:'./app-src/app.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'dist'),
+        publicPath:'dist'
+
+    },
+    module:{
+        rules:
+        [
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+
+            },
+            {
+                test:/\.css$/,
+                loader:'style-loader!css-loader'
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }         
+        ]
+    },
+    plugins
+}
